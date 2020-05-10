8ea156ab3eb226ae88829bdd2fd25cb5f4e578f4
Mehedi Zamadar <meheditcf@gmail.com>
2020-05-10 16:53:14 +0600

    model creating and sql update

---
+++ BooklistApp/Model/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooklistApp.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //empty contructor, but the parameter is needed for dependency injection
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+                
+        }
+        public DbSet<Booklist> booklist { get; set; }
+    }
+}
